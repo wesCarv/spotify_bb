5fd61abf9845e83f97a9da7e2f0339c3e0ad75e4
Nicolai Hygino <nicolaihygino2000@gmail.com>
2023-09-01 13:15:21 -0300

    feat: instala react-router-dom, styled-components e axios

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,38 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
+    'plugin:react/recommended'
   ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['prettier', '@typescript-eslint', 'react'],
-  rules: {},
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off'
+  }
 }
